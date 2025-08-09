f3d0fb25dcb9fabdc7936a2782934ca7dff98ff7
Waruni Prashangi <158755842+SL-winu33@users.noreply.github.com>
2025-08-09 19:54:54 +0530

    upload footer

---
+++ footer.php
@@ -0,0 +1,45 @@
+
+
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="contact.php">contact</a>
+            <a href="shop.php">shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="orders.php">my orders</a>
+            <a href="cart.php">my cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i>+9470-2569874 </p>
+            <p> <i class="fas fa-phone"></i> +9447-8456789</p>
+            <p> <i class="fas fa-envelope"></i>winujayasooriya@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Hambanthota, Sri Lanka  </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+
+    </div>
+
+    <div class="credit" style="color: #FF1493;">🌹🌷Thank you for letting us add color to your world! 🌹🌷</div>
+
+
+</section>
